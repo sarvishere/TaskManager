d127e69b9ee38ebabc0c6232de052df533808011
AmirHD <amirhd.dev@gmail.com>
2024-02-13 02:24:06 +0330

    Initial project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className=" font-iranyekan font-heavy text- text-indigo-primary dark:text-cyan-primary">
+        سلام بچه های بهترین گروه بوتکمپ کوئرا
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,104 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "media",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      iranyekan: ["IRANYekan", "sans-serif"],
+    },
+    fontWeight: {
+      thin: "100",
+      ultraLight: "200",
+      light: "300",
+      normal: "400",
+      medium: "500",
+      demiBold: "550",
+      bold: "600",
+      extraBold: "700",
+      black: "800",
+      extraBlack: "900",
+      heavy: "950",
+    },
+
+    extend: {
+      fontSize: {
+        lh: ["32px", "36px"], // => Heading L
+        mh: ["28px", "34px"], // => Heading M
+        /*
+         * 2xl => Heading S || Body XL
+         * xl => Heading XS || Body L
+         * base => Body M
+         * sm => Body S
+         * xs => Body xs
+         */
+      },
+      colors: {
+        gray: {
+          primary: "#868E96",
+          secondary: "#F1F3F5",
+          darker: "#343A40",
+        },
+        red: {
+          primary: "#FA5252",
+          secondary: "#FFE3E3",
+        },
+        pink: {
+          primary: "#E64980",
+          secondary: "#FFDEEB",
+        },
+        grape: {
+          primary: "#BE4BDB",
+          secondary: "#F3D9FA",
+        },
+        violet: {
+          primary: "#7950F2",
+          secondary: "#E5DBFF",
+        },
+        indigo: {
+          primary: "#4C6EF5",
+          secondary: "#DBE4FF",
+        },
+        blue: {
+          primary: "#228BE6",
+          secondary: "#D0EBFF",
+        },
+        cyan: {
+          primary: "#15AABF",
+          secondary: "#C5F6FA",
+        },
+        teal: {
+          primary: "#12B886",
+          secondary: "#C3FAE8",
+        },
+        brand: {
+          primary: "#208D8E",
+          secondary: "#C2F7FA",
+        },
+        green: {
+          primary: "#40C057",
+          secondary: "#D3F9D8",
+        },
+        lime: {
+          primary: "#82C91E",
+          secondary: "#E9FAC8",
+        },
+        yellow: {
+          primary: "#FAB005",
+          secondary: "#FFF3BF",
+        },
+        ornage: {
+          primary: "#FD7E14",
+          secondary: "#FFE8CC",
+        },
+      },
+      /* Spacing:
+       * 2 => 8px
+       * 4 => 16px
+       * 6 => 24px
+       * 8 => 32px
+       * 10 => 40px
+       */
+    },
+  },
+  plugins: [],
+};
