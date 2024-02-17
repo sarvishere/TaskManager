3171973eee58624c9927b2c18c188263b156928a
Amir Shahidi <shahidi.amir78@gmail.com>
2024-02-18 01:56:58 +0330

    Feat: Add Heading component

---
+++ src/components/Heading.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { fontWeight, colors, Size } from "./SharedComponentStyles/sharedStyles";
+
+enum TextAlign {
+  center = "text-center",
+  left = "text-left",
+  right = "text-right",
+}
+interface Props {
+  as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  align?: keyof typeof TextAlign;
+  children: string;
+  color?: keyof typeof colors;
+  colorVariant?: "primary" | "secondary";
+  size?: keyof typeof Size;
+  weight?: keyof typeof fontWeight;
+  className?: string;
+}
+
+const Heading = ({
+  as,
+  children,
+  color = "indigo",
+  colorVariant = "primary",
+  size = "XS",
+  weight = "800",
+  align = "right",
+  className,
+}: Props) => {
+  return React.createElement(as, {
+    className: `font-iranyekan ${Size[size]} ${colors[color][colorVariant]} ${fontWeight[weight]} ${className} ${TextAlign[align]}`,
+    children,
+  });
+};
+
+export default Heading;
