2115f9d44debda1025cbf348dbb967ad3144f23f
Amir Shahidi <shahidi.amir78@gmail.com>
2024-02-18 03:13:11 +0330

    Feat: Add a reusable Flex component

---
+++ src/components/Flex.tsx
@@ -0,0 +1,67 @@
+import { HTMLAttributes, ReactNode } from "react";
+
+enum Size {
+  XS = "gap-2",
+  S = "gap-4",
+  M = "gap-6",
+  L = "gap-8",
+  XL = "gap-10",
+}
+enum FlexDirection {
+  row = "flex-row",
+  row_reverse = "flex-row-reverse",
+  col = "flex-col",
+  col_reverse = "flex-col-reverse",
+}
+
+const justifyMap = {
+  start: "justify-start",
+  end: "justify-end",
+  center: "justify-center",
+  between: "justify-between",
+  around: "justify-around",
+  evenly: "justify-evenly",
+};
+
+const alignMap = {
+  start: "items-start",
+  end: "items-end",
+  center: "items-center",
+  baseline: "items-baseline",
+  stretch: "items-stretch",
+};
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+  direction?: keyof typeof FlexDirection;
+  gap?: keyof typeof Size;
+  justifyContent?: "start" | "end" | "center" | "between" | "around" | "evenly";
+  alignItems?: "start" | "end" | "center" | "baseline" | "stretch";
+}
+
+const Flex = ({
+  children,
+  justifyContent = "start",
+  alignItems = "start",
+  direction = "row",
+  gap = "XS",
+  className,
+  ...rest
+}: Props) => {
+  return (
+    <div
+      className={`w-full flex 
+      ${Size[gap]}
+      ${FlexDirection[direction]} 
+      ${justifyContent ? justifyMap[justifyContent] : "justify-start"}
+      ${alignItems ? alignMap[alignItems] : "items-start"}
+      ${className}
+      `}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Flex;
