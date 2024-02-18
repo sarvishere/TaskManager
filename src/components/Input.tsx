eba31e47f28d6234be3e11dbc756339f88e47efd
Amir Shahidi <shahidi.amir78@gmail.com>
2024-02-18 03:47:25 +0330

    Feat: Add input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Flex from "./Flex";
+import { colors, BodySize } from "./SharedComponentStyles/sharedStyles";
+
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  labelFontSize?: keyof typeof BodySize;
+  labelColor?: keyof typeof colors;
+  labelColorVariant?: "primary" | "secondary";
+}
+
+const Input = React.forwardRef<HTMLInputElement, Props>(
+  (
+    {
+      label,
+      className,
+      id,
+      labelColor = "default",
+      labelColorVariant = "primary",
+      labelFontSize = "S",
+      ...rest
+    },
+    ref
+  ) => {
+    return (
+      <Flex direction="col" gap="XS">
+        <label
+          className={`font-iranyekan block ${BodySize[labelFontSize]} ${colors[labelColor][labelColorVariant]}`}
+          htmlFor={id}
+        >
+          {label}
+        </label>
+        <input
+          ref={ref}
+          id={id}
+          className={`w-full ${colors["default"]["primary"]} rounded-[6px] border border-solid border-gray-aaa h-[40px] ${className}`}
+          type="text"
+          {...rest}
+        />
+      </Flex>
+    );
+  }
+);
+
+export default Input;
