c280ce20d1b7bd9097998c13fa14ac9847e5ca10
Amir Shahidi <shahidi.amir78@gmail.com>
2024-02-21 05:33:15 +0330

    refactor(PasswordResetForm): Organize code structure

--- src/components/PasswordResetForm.tsx/PasswordResetForm.tsx
+++ src/components/PasswordResetForm.tsx/PasswordResetForm.tsx
@@ -1,29 +1,28 @@
-import Button from "../Button";
-import Card from "../Card";
-import CheckBox from "../CheckBox";
-import Flex from "../Flex";
-import Heading from "../Heading";
-import Input from "../Input";
-import Link from "../Link";
+import Button from "../ui/Button";
+import Card from "../ui/Card";
+import Flex from "../ui/Flex";
+import Heading from "../ui/Heading";
+import Input from "../ui/Input";
+import Link from "../ui/Link";
 
 const RegisterForm = () => {
   return (
-    <form>
-      <Card>
-        <Heading align="center" className="mb-8" as="h2" size="L">
-          فراموشی رمز عبور
-        </Heading>
+    <Card>
+      <Heading align="center" className="mb-8" as="h2" size="L">
+        فراموشی رمز عبور
+      </Heading>
+      <form>
         <Flex gap="M" direction="col" alignItems="center">
           <Input type="email" id="email" label="ایمیل خود را وارد کنید" />
-          <Button color="brand" size="full">
+          <Button type="submit" color="brand" size="full">
             دریافت ایمیل بازیابی رمز عبور
           </Button>
-          <Link color="brand" to="/login" size="M" weight="800">
+          <Link color="brand" to="/" size="M" weight="800">
             بازگشت
           </Link>
         </Flex>
-      </Card>
-    </form>
+      </form>
+    </Card>
   );
 };
 
