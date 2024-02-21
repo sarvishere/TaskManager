2f74673dbfa70d331d1ab79a52627f729b07f4ca
Amir Shahidi <shahidi.amir78@gmail.com>
2024-02-21 12:59:28 +0330

    refactor(route): Refactor the route to a cleaner route by using nested routes

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,22 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 import Test from "../pages/test";
 import AuthenticationLayout from "../layout/AuthenticationLayout";
-
+import HomePage from "../pages/HomePage";
+import RegisterPage from "../pages/RegisterPage";
+import PasswordResetPage from "../pages/PasswordResetPage";
+import PrivateRoutes from "./PrivateRoutes";
+import UsersPage from "../pages/UsersPage";
 
 const router = createBrowserRouter([
-    {
-        path : "/",
-        element: <AuthenticationLayout />
-    },
-    {
-        path: "/register",
-        element: <AuthenticationLayout />
-    },
-    {
-        path :'/test',
-        element: <Test/>
-    },
+  {
+    path: "/",
+    element: <AuthenticationLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "register", element: <RegisterPage /> },
+      { path: "forgot-password", element: <PasswordResetPage /> },
+    ],
+  },
+  {
+    element: <PrivateRoutes />,
+    children: [{ path: "users", element: <UsersPage /> }],
+  },
+  {
+    path: "/test",
+    element: <Test />,
+  },
 ]);
 
 export default router;
-
