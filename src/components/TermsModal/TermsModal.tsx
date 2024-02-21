e94c64671fd7b0978d2210e78eaaf31708752199
Amir Shahidi <shahidi.amir78@gmail.com>
2024-02-21 05:39:07 +0330

    fix(TermsModal): fix the positioning of the TermsModal

--- src/components/TermsModal/TermsModal.tsx
+++ src/components/TermsModal/TermsModal.tsx
@@ -1,8 +1,8 @@
 import CloseIcon from "../../assets/Icons/CloseIcon.svg";
-import Button from "../Button";
-import Flex from "../Flex";
-import Heading from "../Heading";
-import Text from "../Text";
+import Button from "../ui/Button";
+import Flex from "../ui/Flex";
+import Heading from "../ui/Heading";
+import Text from "../ui/Text";
 
 interface Props {
   visible?: boolean;
@@ -14,7 +14,7 @@ const TermsModal = ({ visible, onClose }: Props) => {
     <div
       className={`${
         !visible && "hidden"
-      } fiexed z-50 inset-0 bg-[#17191B]/60 backdrop-blur-sm flex justify-center items-center`}
+      } fixed h-screen w-screen z-50 inset-0 bg-[#17191B]/60 backdrop-blur-sm flex justify-center items-center`}
     >
       <div className="relative bg-white max-w-[800px] w-full rounded-[20px] pt-6 pr-6 pb-8 pl-6 shadow-[0_30px_60px_-30px_rgba(0,0,0,0.3)]">
         <Button onClick={onClose} asChild>
