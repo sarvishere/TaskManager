00b00a2c537bd6ab113fa80afeb002ec8183d3e4
Amir Shahidi <shahidi.amir78@gmail.com>
2024-02-21 11:58:52 +0330

    fix(layout): Fix a bug in navigation and remove redundant state

--- src/layout/AuthenticationLayout/index.tsx
+++ src/layout/AuthenticationLayout/index.tsx
@@ -1,5 +1,6 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode } from "react";
 import { useLocation } from "react-router-dom";
+import Link from "../../components/ui/Link";
 
 interface IAuthenticationLayoutProps {
   children?: ReactNode;
@@ -8,24 +9,25 @@ const AuthenticationLayout: React.FC<IAuthenticationLayoutProps> = ({
   children,
 }) => {
   const location = useLocation();
-  const [isRegistered, setIsRegistered] = useState(true);
 
-  useEffect(() => {
-    location.pathname === "/" && setIsRegistered(false);
-  }, [location]);
   return (
     <main className="m-0 p-0 h-screen w-screen overflow-hidden flex flex-col font-iranyekan relative">
       <header className="flex justify-between items-center m-20 h-[45px]">
         <h1 className="logo font-iranyekan h-fit">کوئرا تسک منیجر</h1>
         <div className="flex items-center">
           <h6 className="text-base font-medium ml-[7px]">
-            {isRegistered === false
+            {location.pathname === "/"
               ? "ثبت نام نکرده ای؟"
               : "قبلا ثبت نام کرده ای؟"}
           </h6>
-          <button className="bg-brand-primary w-[95px] h-10 p-[10px] rounded-[6px] text-white text-sm font-extrabold">
-            {isRegistered === false ? "ثبت نام" : "ورود"}
-          </button>
+          <Link
+            className="text-white text-center p-[10px] bg-brand-primary w-[95px] h-[40px] rounded-[6px]"
+            weight="800"
+            size="S"
+            to={location.pathname === "/" ? "/register" : "/"}
+          >
+            {location.pathname === "/" ? "ثبت نام" : "ورود"}
+          </Link>
         </div>
       </header>
       <section className="flex justify-center items-center z-50">
