4e958bf2504168324d25c3c2953a712724aca558
Amir Shahidi <shahidi.amir78@gmail.com>
2024-02-21 05:35:34 +0330

    feat(RegisterForm): Add state for showing TermsModal

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,18 +1,29 @@
-import Button from "../Button";
-import Card from "../Card";
-import CheckBox from "../CheckBox";
-import Flex from "../Flex";
-import Heading from "../Heading";
-import Input from "../Input";
-import Link from "../Link";
+import { useState } from "react";
+import Button from "../ui/Button";
+import Card from "../ui/Card";
+import CheckBox from "../ui/CheckBox";
+import Flex from "../ui/Flex";
+import Heading from "../ui/Heading";
+import Input from "../ui/Input";
+import Text from "../ui/Text";
+import { TermsModal } from "../TermsModal";
 
 const RegisterForm = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <form>
-      <Card>
-        <Heading align="center" className="mb-8" as="h2" size="L">
-          ثبت‌نام در کوئرا تسک منیجر
-        </Heading>
+    <Card>
+      <Heading align="center" className="mb-8" as="h2" size="L">
+        ثبت‌نام در کوئرا تسک منیجر
+      </Heading>
+      <form>
         <Flex gap="M" direction="col">
           <Input id="username" label="نام کاربری" />
           <Input type="email" id="email" label="ایمیل" />
@@ -21,19 +32,22 @@ const RegisterForm = () => {
             id="rules"
             label={
               <span>
-                <Link to="" size="M" underline>
-                  قوانین و مقررات
-                </Link>{" "}
+                <Button onClick={handleOpenModal} asChild>
+                  <Text className="underline underline-offset-8" size="M">
+                    قوانین و مقررات
+                  </Text>
+                </Button>{" "}
                 را می‌پذیرم.
               </span>
             }
           />
-          <Button color="brand" size="full">
+          <TermsModal visible={showModal} onClose={handleCloseModal} />
+          <Button type="submit" color="brand" size="full">
             ثبت‌نام
           </Button>
         </Flex>
-      </Card>
-    </form>
+      </form>
+    </Card>
   );
 };
 
