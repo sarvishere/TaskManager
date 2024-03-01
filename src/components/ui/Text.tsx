421c446cdd7e253aefe3211709150711c686ddd6
Amir Shahidi <shahidi.amir78@gmail.com>
2024-03-01 16:48:45 +0330

    feat: Add support for multiple strings in the Text component

--- src/components/ui/Text.tsx
+++ src/components/ui/Text.tsx
@@ -4,7 +4,7 @@ import {
   fontWeight,
 } from "../SharedComponentStyles/sharedStyles";
 interface Props {
-  children?: string;
+  children?: string | string[];
   color?: keyof typeof colors;
   colorVariant?: "primary" | "secondary";
   size?: keyof typeof BodySize;
