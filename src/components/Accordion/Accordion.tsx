d13e446bc47415f42ffa2c957408cb3d388d4e7d
AmirHD <amirhd.dev@gmail.com>
2024-03-04 16:19:16 +0330

    feet: add HOC accordion & its hook

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,41 @@
+import { animated, useSpring } from "react-spring";
+import { useAccordionStore } from "../../utils/hooks/useAccordionStore";
+import Flex from "../ui/Flex";
+
+interface AccordionProps {
+  id: string;
+  head: React.ReactNode;
+  children: React.ReactNode;
+}
+
+const Accordion = ({ id, head, children }: AccordionProps) => {
+  const { openAccordions, toggleAccordion } = useAccordionStore();
+  const isOpen = openAccordions.includes(id);
+
+  const animation = useSpring({
+    from: {
+      height: 0,
+      opacity: 0,
+    },
+    to: {
+      height: isOpen ? "auto" : 0,
+      opacity: isOpen ? 1 : 0,
+    },
+    config: { friction: 100 },
+  });
+
+  return (
+    <Flex direction="col">
+      <div
+        className="w-full cursor-pointer py-5"
+        onClick={() => toggleAccordion(id)}
+      >
+        {head}
+      </div>
+      <animated.div style={animation} className="w-full overflow-hidden">
+        {children}
+      </animated.div>
+    </Flex>
+  );
+};
+export default Accordion;
