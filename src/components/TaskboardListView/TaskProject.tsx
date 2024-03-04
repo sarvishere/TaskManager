8eb49e7d969b394d309e388ac036f0d440d77a2c
AmirHD <amirhd.dev@gmail.com>
2024-03-04 16:29:39 +0330

    feat: add seperated UI components for listing task

---
+++ src/components/TaskboardListView/TaskProject.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from "react";
+import Accordion from "../Accordion/Accordion";
+import Flex from "../ui/Flex";
+import Icon from "../ui/Icon";
+import Heading from "../ui/Heading";
+import { useAccordionStore } from "../../utils/hooks/useAccordionStore";
+
+interface TaskProjectProps {
+  id: string;
+  title: string;
+}
+
+const TaskProject: FC<TaskProjectProps> = ({ id, title }) => {
+  const { openAccordions } = useAccordionStore();
+  const isOpen = openAccordions.includes(id);
+  return (
+    <Flex alignItems="center" className=" cursor-pointer">
+      <Icon
+        iconName="ChevronDown"
+        className={`border-2 border-gray-darker rounded-full w-6 h-6 ${
+          isOpen ? "" : "rotate-180"
+        }`}
+      />
+      <Heading as="h3">{title}</Heading>
+    </Flex>
+  );
+};
+export default TaskProject;
