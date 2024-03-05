c2da55aaab9baa3415b9945b39a728718373bbca
Amir Shahidi <shahidi.amir78@gmail.com>
2024-03-05 21:38:56 +0330

    feat(service): Add Axios interceptors for appending access token to requests

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,38 @@
 import axios, { AxiosResponse } from "axios";
+import TokenManager from "./token-service";
 
 const axiosInstance = axios.create({
   baseURL: "http://185.8.174.74:8000",
 });
+
+axiosInstance.interceptors.request.use((config) => {
+  const accessToken = TokenManager.getAccessToken();
+  if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
+
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalRequest = error.config;
+
+    try {
+      if (error.response?.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+
+        const newAccessToken = await TokenManager.refreshToken();
+
+        TokenManager.setAccessToken(newAccessToken);
+
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return axios(originalRequest);
+      }
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+);
 
 class APIClient<TData, TResponse> {
   constructor(readonly endpoint: string) {}
