29b0e532131d0376cd27a20023fe391a2abef04a
Amir Shahidi <shahidi.amir78@gmail.com>
2024-03-08 18:06:39 +0330

    feat(RegisterForm): Add form validation logic

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { FieldError, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 import Button from "../ui/Button";
 import Card from "../ui/Card";
 import CheckBox from "../ui/CheckBox";
@@ -8,8 +11,32 @@ import Input from "../ui/Input";
 import Text from "../ui/Text";
 import { TermsModal } from "../TermsModal";
 
+const validationErrors = {
+  username: "نام کاربری باید حداقل ۳ کاراکتر داشته باشد.",
+  email: "لطفاً یک ایمیل معتبر وارد کنید.",
+  password: "رمز عبور باید حداقل ۸ کاراکتر داشته باشد.",
+  termsAndConditions: "برای ثبت‌نام، قوانین را باید بپذیرید.",
+};
+
+const registrationSchema = z.object({
+  username: z.string().min(3, { message: validationErrors.username }),
+  email: z.string().email({ message: validationErrors.email }),
+  password: z.string().min(8, { message: validationErrors.password }),
+  termsAndCondition: z.boolean().refine((value) => value === true, {
+    message: validationErrors.termsAndConditions,
+  }),
+});
+type FormData = z.infer<typeof registrationSchema>;
+
 const RegisterForm = () => {
   const [showModal, setShowModal] = useState(false);
+  const {
+    register,
+    formState: { errors, isSubmitting },
+    handleSubmit,
+  } = useForm<FormData>({ resolver: zodResolver(registrationSchema) });
+
+  const onSubmit = (data: FormData) => {};
 
   const handleOpenModal = () => {
     setShowModal(true);
@@ -23,12 +50,37 @@ const RegisterForm = () => {
       <Heading align="center" className="mb-8" as="h2" size="L">
         ثبت‌نام در کوئرا تسک منیجر
       </Heading>
-      <form>
-        <Flex gap="M" direction="col">
-          <Input id="username" label="نام کاربری" />
-          <Input type="email" id="email" label="ایمیل" />
-          <Input type="password" id="password" label="رمز عبور" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Flex gap={`${errors ? "S" : "M"}`} direction="col">
+          <Input
+            className={getErrorStyles(errors.username)}
+            {...register("username")}
+            id="username"
+            label="نام کاربری"
+          />
+          <ErrorMessage error={errors.username} />
+
+          <Input
+            className={getErrorStyles(errors.email)}
+            {...register("email")}
+            type="email"
+            id="email"
+            label="ایمیل"
+          />
+          <ErrorMessage error={errors.email} />
+
+          <Input
+            className={getErrorStyles(errors.password)}
+            {...register("password")}
+            type="password"
+            id="password"
+            label="رمز عبور"
+          />
+          <ErrorMessage error={errors.password} />
+
           <CheckBox
+            {...register("termsAndCondition", { required: true })}
+            className={getErrorStyles(errors.termsAndCondition)}
             id="rules"
             label={
               <span>
@@ -41,14 +93,38 @@ const RegisterForm = () => {
               </span>
             }
           />
+          <ErrorMessage error={errors.termsAndCondition} />
+
           <TermsModal visible={showModal} onClose={handleCloseModal} />
-          <Button type="submit" color="brand" size="full">
+          <Button
+            disabled={isSubmitting}
+            type="submit"
+            color="brand"
+            size="full"
+          >
             ثبت‌نام
           </Button>
         </Flex>
       </form>
     </Card>
   );
+};
+
+const getErrorStyles = (error: FieldError | undefined) => {
+  return error ? "border-red-600 border-2" : "";
+};
+
+const ErrorMessage: React.FC<{
+  error: FieldError | undefined;
+}> = ({ error }) => {
+  if (error) {
+    return (
+      <Text color="red" size="S">
+        {error.message}
+      </Text>
+    );
+  }
+  return null;
 };
 
 export default RegisterForm;
