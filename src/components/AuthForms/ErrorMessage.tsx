7dea634a13ab7dd6af8635b122bb70cb4e265353
Amir Shahidi <shahidi.amir78@gmail.com>
2024-03-08 18:35:51 +0330

    feat(RegisterForm): Extract a reusable ErrorMessage component for rendering errors below forms

---
+++ src/components/AuthForms/ErrorMessage.tsx
@@ -0,0 +1,17 @@
+import { FieldError } from "react-hook-form";
+import Text from "../ui/Text";
+
+const ErrorMessage: React.FC<{
+  error: FieldError | undefined;
+}> = ({ error }) => {
+  if (error) {
+    return (
+      <Text color="red" size="S">
+        {error.message}
+      </Text>
+    );
+  }
+  return null;
+};
+
+export default ErrorMessage;
