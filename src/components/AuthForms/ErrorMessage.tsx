9961abce8da7b6ed42e45dcb198ad8779a030fd1
Amir Shahidi <shahidi.amir78@gmail.com>
2024-03-10 04:09:49 +0330

    feat(AuthForms): Add a reusable component for showing validation errors below form fields

--- src/components/AuthForms/ErrorMessage.tsx
+++ src/components/AuthForms/ErrorMessage.tsx
@@ -6,7 +6,11 @@ const ErrorMessage: React.FC<{
 }> = ({ error }) => {
   if (error) {
     return (
-      <Text className="self-start" color="red" size="S">
+      <Text
+        className="mt-1 relative self-start z-20 shadow-[0_0_5px_#f02236] bg-red-500 before:-z-10 rounded-lg text-white p-2 before:content-[''] before:w-4 before:h-4 before:rotate-45 before:absolute before:-top-[20%] before:right-5 before:bg-red-500"
+        color="red"
+        size="S"
+      >
         {error.message}
       </Text>
     );
