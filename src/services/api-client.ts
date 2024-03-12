b20a1f666b96cc263380cd410ef37e0aedd83765
Amir Shahidi <shahidi.amir78@gmail.com>
2024-03-12 16:15:48 +0330

    fix(api-client): Correct access token refreshing logic

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,45 +1,48 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, {
+  AxiosRequestConfig,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from "axios";
 import TokenManager from "./token-service";
 
+const baseURL = "http://185.8.174.74:8000";
 const axiosInstance = axios.create({
-  baseURL: "http://185.8.174.74:8000",
+  baseURL,
 });
 
 axiosInstance.interceptors.request.use(
-  (config) => {
-    const accessToken = TokenManager.getAccessToken();
-    if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
+  async (config: InternalAxiosRequestConfig) => {
+    if (config.url === "/accounts/login/") return config;
+    try {
+      const accessToken = TokenManager.getAccessToken();
+      const refreshToken = TokenManager.getRefreshToken();
+
+      if (!accessToken) {
+        throw new Error("Access token not found");
+      }
+
+      const decodedAccessToken = JSON.parse(atob(accessToken.split(".")[1]));
+      const isExpired = new Date(decodedAccessToken.exp * 1000) < new Date();
+
+      if (isExpired && refreshToken) {
+        const {
+          data: { access },
+        } = await axios.post<{ access: string }>(
+          `${baseURL}/accounts/refresh/`,
+          { refresh: refreshToken }
+        );
+        TokenManager.setAccessToken(access);
+        config.headers.Authorization = `Bearer ${access}`;
+      } else {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
+    } catch (error) {
+      window.location.href = "/";
+    }
 
     return config;
   },
   (error) => Promise.reject(error)
-);
-
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-
-    if (
-      error.response.status === 401 &&
-      !originalRequest._retry &&
-      TokenManager.getRefreshToken()
-    ) {
-      originalRequest._retry = true;
-
-      try {
-        const newAccessToken = TokenManager.refreshToken();
-
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-        return axiosInstance(originalRequest);
-      } catch (error) {
-        console.error("Error refreshing token:", error);
-        window.location.href = "/";
-      }
-    }
-
-    return Promise.reject(error);
-  }
 );
 
 class APIClient<TData, TResponse> {
