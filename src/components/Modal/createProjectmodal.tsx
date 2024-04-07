bc5c1ef661c15ff6be3d701d5f1afbeccc15fb74
sarvishere <sarvenazmoayer1400@gmail.com>
2024-04-07 18:57:06 +0330

    fix bug

--- src/components/Modal/createProjectmodal.tsx
+++ src/components/Modal/createProjectmodal.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import Icon from '../ui/Icon';
-import Button from '../ui/Button';
+import React from "react";
+import Icon from "../ui/Icon";
 
 interface CreateProjectModalProps {
   onClose: () => void;
@@ -12,46 +11,47 @@ const CreateProjectModal: React.FC<CreateProjectModalProps> = ({ onClose }) => {
   };
 
   return (
-    <div className='w-[184px] h-[204px] rounded-lg bg-white justify-center flex flex-col items-center'>
+    <div className="w-[184px] h-[204px] rounded-lg bg-white justify-center flex flex-col items-center">
       <button onClick={handleClose}>
-        <Icon iconName="Close"  />
+        <Icon iconName="Close" />
       </button>
 
-     <div >
-        <div className='flex '>
+      <div>
+        <div className="flex ">
           <Icon iconName="Add" />
           <p> ساخت پروژه جدید </p>
         </div>
 
-        <div className='flex mt-2'>
+        <div className="flex mt-2">
           <Icon iconName="Edit" />
           <p> ویرایش نام ورک اسپیس</p>
         </div>
 
-        <div className='flex mt-2'>
+        <div className="flex mt-2">
           <Icon iconName="ColorPalette" />
           <p>  ویرایش رنگ</p>
         </div>
 
-        <div className='flex mt-2'>
+        <div className="flex mt-2">
           <Icon iconName="Link" />
           <p> کپی لینک</p>
         </div>
 
-        <div className='flex mt-2'>
+        <div className="flex mt-2">
           <Icon iconName="Remove" />
           <p> حذف</p>
         </div>
-        </div>
+      </div>
       <div>
-
-      <div className='flex mt-2'>
-      <button type="submit" className="submit-button w-[160px] h-[36px] rounded bg-[#208D8E] flex items-center justify-center">
-        <Icon iconName="Share" />
-        <p className="ml-1"> اشتراک گذاری</p>
-      </button>  
-    </div>
-
+        <div className="flex mt-2">
+          <button
+            type="submit"
+            className="submit-button w-[160px] h-[36px] rounded bg-[#208D8E] flex items-center justify-center"
+          >
+            <Icon iconName="Share" />
+            <p className="ml-1"> اشتراک گذاری</p>
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,11 +1,13 @@
-import CreateProjectModal from "../../components/Modal/createprojectmodal";
+import CreateProjectModal from "../../components/Modal/createProjectmodal";
 
 const Test = () => {
   return (
-    <CreateProjectModal onClose={function (): void {
-      throw new Error("Function not implemented.");
-    } }/>
-  )
+    <CreateProjectModal
+      onClose={function (): void {
+        throw new Error("Function not implemented.");
+      }}
+    />
+  );
 };
 
 export default Test;
